d0a47f065b69f35d9d20135781de4d3183f49382
LyTrungHai <trunghai1610@gmail.com>
2021-08-05 11:57:30 +0700

    fix

--- js/baitap1.js
+++ js/baitap1.js
@@ -1,8 +1,24 @@
-// Đề bài : viết chương trình tính lương, biết lương 1 ngày là : 100.000 
+// Đề bài : viết chương trình tính lương, biết lương 1 ngày là : 100.000
 
+// bài làm
 
-var luongMotNgay = 100000, soNgayLam = 30 , 
-tongLuong = 0;
+/*khối 1 : input
+ *lương một ngày
+ *số ngày làm
+ */
+var luongMotNgay = 100000,
+  soNgayLam = 30,
+  tongLuong = 0;
 
-var tongLuong = (luongMotNgay*soNgayLam)
-console.log("tổng lương là: " + (tongLuong) + "vnđ")
+/**khối 2: xử lý
+ * viết công thức tính tổng lương : Tổng lương = lương một ngày * số ngày làm
+ */
+var tongLuong = luongMotNgay * soNgayLam;
+
+/**khối 3:output
+ * tổng lương
+ */
+console.log(
+  "lương một ngày là: " + luongMotNgay + "số ngày làm là: " + soNgayLam,
+  " tổng lương là: " + tongLuong + "vnđ"
+);
